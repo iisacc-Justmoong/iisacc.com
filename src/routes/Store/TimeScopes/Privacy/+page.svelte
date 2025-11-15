371aee5cb7b35a9e26baf09563bf322054ff4468
iisacc-Justmoong <andudyun0504@icloud.com>
2025-11-16 00:40:33 +0900

    New Vincent page

--- src/routes/Store/TimeScopes/Privacy/+page.svelte
+++ src/routes/Store/TimeScopes/Privacy/+page.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <Header/>
+
 <h1>Privacy Policy</h1>
 <p>Last updated: June 17, 2025</p>
 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
